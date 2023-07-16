555d51a0a341acc608c209317e995ac9e1f15ca2
Bharath-Sethuraman <135990314+Bharath-Sethuraman@users.noreply.github.com>
2023-07-16 19:16:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -155,7 +155,7 @@ app.get("/about",function(req,res){
 
 let port = process.env.PORT;
 if (port == null || port == "") {
- port = 3000;
+ port = 3001;
 }
 app.listen(port, function() {
  console.log("Server started successfully");
